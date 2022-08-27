c98e60fd3121f288c1c67cbc0b44b1fbfcaadef2
zhangzhouzhe <zhangzhouzhe@beisen.com>
2022-08-27 22:44:53 +0800

    Init

---
+++ MyGrpc/MyGrpc.Client/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Grpc.Net.Client;
+using MyGrpc;
+using static MyGrpc.Greeter;
+
+GrpcChannel grpcChannel = GrpcChannel.ForAddress("https://localhost:7150");
+GreeterClient greeterClient = new GreeterClient(grpcChannel);
+
+HelloReply helloReply = greeterClient.SayHello(new HelloRequest
+{
+    Name = "张三"
+});
+
+
+Console.WriteLine(helloReply.Message);
+Console.ReadLine();
